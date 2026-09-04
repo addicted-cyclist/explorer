a6b1ef646516c3a4cc8d3f70a1d29beef46b5c10
addicted.cyclist <addicted.cyclist@gmail.com>
2026-09-04 18:57:06 +0700

    connect calendar_entry with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :routes, dependent: :destroy
+  has_many :calendar_entries, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 100 }
 
