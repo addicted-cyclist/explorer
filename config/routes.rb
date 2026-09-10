9369b761abea8f01760106d84a5e823edebe8a2f
addicted.cyclist <addicted.cyclist@gmail.com>
2026-09-10 21:27:44 +0700

    create all CRUD exept NEW for routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
     root "pages#landing", as: :unauthenticated_root
   end
 
-  resources :routes
+  resources :routes, except: %i[new] do
+    member { patch :toggle_completed }
+  end
 
   resource :calendar, only: %i[show] do
     collection do
